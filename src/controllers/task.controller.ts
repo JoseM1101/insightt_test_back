b2616434a4d85afd7a2b066d3d1d136b7a1791be
Jose Molina <josemolina110101@gmail.com>
2025-12-19 21:30:17 -0400

    task controller implementation

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,31 +1,47 @@
 import { Request, Response } from "express"
 import * as taskService from "../services/task.service"
-import type { TaskParams } from "../types"
 
-export const getTasks = async (_req: Request, res: Response) => {
-  const tasks = await taskService.getTasks()
+const requireId = (req: Request, res: Response): string | null => {
+  const { id } = req.params
+  if (!id) {
+    res.status(400).json({ message: "id is required" })
+    return null
+  }
+  return id
+}
+
+export const getTasks = async (req: Request, res: Response) => {
+  if (!req.params.userId) return
+
+  const tasks = await taskService.getTasks(req.params.userId)
   res.json(tasks)
 }
 
 export const createTask = async (req: Request, res: Response) => {
-  const task = await taskService.createTask(req.body)
+  const task = await taskService.createTask(req.body, "user-1")
   res.status(201).json(task)
 }
 
-export const updateTask = async (req: Request<TaskParams>, res: Response) => {
-  const task = await taskService.updateTask(req.params.id, req.body)
+export const updateTask = async (req: Request, res: Response) => {
+  const id = requireId(req, res)
+  if (!id) return
+
+  const task = await taskService.updateTask(id, req.body)
   res.json(task)
 }
 
-export const deleteTask = async (req: Request<TaskParams>, res: Response) => {
-  await taskService.deleteTask(req.params.id)
+export const deleteTask = async (req: Request, res: Response) => {
+  const id = requireId(req, res)
+  if (!id) return
+
+  await taskService.deleteTask(id)
   res.status(204).send()
 }
 
-export const toggleTaskCompleted = async (
-  req: Request<TaskParams>,
-  res: Response
-) => {
-  const task = await taskService.toggleTaskCompleted(req.params.id)
+export const toggleTaskCompleted = async (req: Request, res: Response) => {
+  const id = requireId(req, res)
+  if (!id) return
+
+  const task = await taskService.toggleTaskCompleted(id)
   res.json(task)
 }
